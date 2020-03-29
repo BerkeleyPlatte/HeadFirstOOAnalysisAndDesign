d4e9b0081b6c44d8d09cb88685e1a541abb6720f
unknown <berkeleyplatte989@gmail.com>
2020-03-29 16:10:36 -0500

    initial commit

---
+++ Inventory.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Inventory {
+    private List guitars;
+
+    public Inventory() {
+        guitars = new LinkedList();
+    }
+
+    public void addGuitar(String serialNumber, double price, String builder, String model, String type, String backWood,
+            String topWood) {
+        Guitar guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+        guitars.add(guitar);
+    }
+
+    public Guitar getGuitar(String serialNumber) {
+        for (Iterator i = guitars.iterator(); i.hasNext();) {
+            Guitar guitar = (Guitar) i.next();
+            if (guitar.getSerialNumber().equals(serialNumber)) {
+                return guitar;
+            }
+        }
+        return null;
+    }
+
+    public List search(Guitar searchGuitar) {
+        for (Iterator i = guitars.iterator(); i.hasNext();) {
+            Guitar guitar = (Guitar) i.next();
+            if (searchGuitar.getBuilder() != guitar.getBuilder())
+                continue;
+            String model = searchGuitar.getModel().toLowerCase();
+            if ((model != null) && (!model.equals("")) && (!model.equals(guitar.getModel().toLowerCase())))
+                continue;
+            if (searchGuitar.getType() != guitar.getType())
+                continue;
+            if (searchGuitar.getBackWood() != guitar.getBackWood())
+                continue;
+            if (searchGuitar.getTopWood() != guitar.getTopWood())
+                continue;
+        }
+        return null;
+    }
+}
